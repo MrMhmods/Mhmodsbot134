4df314bc66350226a467afa5a304f0382c91b5d1
MrMhmods <52112618+MrMhmods@users.noreply.github.com>
2019-07-08 13:34:39 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,14 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
+const prefix = '$'
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+  if (msg.content === '+test') {
+    msg.reply('شغال!');
   }
 });
 
-client.login('token');
+client.login('NTk3NzM2MTYxMjEzMDIyMjQ5.XSMbWA.hjtYCC6zn2wgA0R0a8tCp2hFkCY');
